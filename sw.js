f87b8a3013a568fba4c25db9338e55f565675b03
abidabderrahim <165683095+abidabderrahim@users.noreply.github.com>
2025-10-02 18:52:28 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,21 @@
-const CACHE_NAME = "sales-app-cache-v1";
-const urlsToCache = [
-  "index.html",
-  "style.css",
-  "script.js",
-  "icon.png",
-  "manifest.json"
+const cacheName = 'sales-app-cache-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/icon.png'
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
